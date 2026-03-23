0dbcbc3c116c86a0076c20095915e46986d19014
Rémi Vezy <VEZY@users.noreply.github.com>
2026-03-23 14:33:50 +0100

    add timing for generating home figure

--- scripts/time_home_figure.jl
+++ scripts/time_home_figure.jl
@@ -21,3 +21,7 @@ display(trial)
 # With java version:  java -jar ./example_1/archimed-lib-2018-0.0.1-SNAPSHOT-jar-with-dependencies.jar example_2/config.yml
 # 2026-03-18 12:03:14.891 - Simulation time 14.448s0.0ms
 # 2026-03-18 12:03:14.892 - maximum memory usage 2277,72 MB
+
+# With Julia: 
+# Single result which took 1.723 s (12.55%) to evaluate,
+#  with a memory estimate of 1.70 GiB, allocs estimate: 12139938
