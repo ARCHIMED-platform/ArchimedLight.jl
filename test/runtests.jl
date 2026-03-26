dd8e4c8c399852c702fd2087392eb2963388c740
Rémi Vezy <VEZY@users.noreply.github.com>
2026-03-26 15:50:33 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,7 @@ using TestItemRunner
 const _DEFAULT_TEST_FILTER = ti -> !(:release in ti.tags)
 
 TestItemRunner.run_tests(joinpath(@__DIR__, ".."); filter=_DEFAULT_TEST_FILTER, verbose=true)
+
+# Infos:
+# Run this for generating figures from the release fixtures VS the reference ones:
+# julia --project=test scripts/render_release_fixture_diffs.jl --all
